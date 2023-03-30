c3521b0ab4fc33cbd3fd7e4bec1ff0905ac11623
CrIxX <cr1xx1991@gmail.com>
2023-03-29 23:11:45 -0600

    fix 01

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server,{
     cors:{
-        origin:'*'
+        origin:'*',
+        credentials:true
     }, 
     transports: ['websocket', 'polling', 'flashsocket']
 });
