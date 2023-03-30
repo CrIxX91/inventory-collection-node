a5f4e6feba43769c0b974c039feda238657e5de4
CrIxX <cr1xx1991@gmail.com>
2023-03-29 21:38:46 -0600

    socket io added

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express');
+// const app = express();
+const http = require('http');
+const { Server } = require("socket.io");
 require('dotenv').config();
 const cors = require("cors");
 const { dbConection } = require('./database/config');
+const Item = require("./models/ItemModel");
+
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server,{
+    cors:{
+        origin:'*'
+    }
+});
+
 dbConection();
 
 app.use(express.static('public'));
@@ -14,10 +26,42 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/inventory', require('./routes/inventory'));
 app.use('/api/brand', require('./routes/brand'));
 
-app.get('/', (req,res)=>{
-    res.send('Init Page');
+// app.get('/', (req,res)=>{
+//     res.send('Init Page');
+// });
+
+io.on('connection',(socket)=>{
+    
+    console.log('connected',socket.id)
+
+    interval = setInterval(() => {
+        Item.find().exec().then(
+            x =>{
+                
+                x.sort(function (a, b) {
+                    if (a.name < b.name) {
+                      return -1;
+                    }
+                    if (a.name > b.name) {
+                      return 1;
+                    }
+                    return 0;
+                  });
+                socket.emit("Figures",x);
+                // console.log(x);
+            } 
+        );
+    }, 1000);
+    // socket.on('message',(msg)=>{
+    //     console.log(msg);
+    // })
+
+    socket.on("disconnect", () => {
+        console.log('disconnect',socket.id); // undefined
+    });
 })
 
-app.listen(process.env.PORT,()=>{
+
+server.listen(process.env.PORT,()=>{
     console.log(`servidor corriendo en puerto ${process.env.PORT}`)
-})
+});
