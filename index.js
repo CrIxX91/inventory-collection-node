bdf0bcf3c6790240f5d84d87360f98c6a19b38ad
CrIxX <cr1xx1991@gmail.com>
2023-03-30 09:26:29 -0600

    delete socket

--- index.js
+++ index.js
@@ -1,28 +1,37 @@
 const express = require('express');
 // const app = express();
-const http = require('http');
-const { Server } = require("socket.io");
+// const http = require('http');
+// const { Server } = require("socket.io");
 require('dotenv').config();
 const cors = require("cors");
 const { dbConection } = require('./database/config');
-const Item = require("./models/ItemModel");
+// const Item = require("./models/ItemModel");
 
 
 const app = express();
-const server = http.createServer(app,{
-    pingTimeout: 60000
-  });
-const io = new Server(server,{
-    cors:{
-        origin:'*'
-        // origin:['http://localhost:3000','https://inventory-collection-cr1xx.vercel.app'],
-        // credentials:true,
-        // methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH'],
-        // allowedHeaders:'*'
-    }, 
-    // transports: ['websocket', 'polling', 'flashsocket'],
+// const server = http.createServer(app,{
+//     pingTimeout: 60000
+//   });
+// const io = new Server(server,{
+//     cors:{
+//         origin:'*',
+//         methods: ["GET", "POST"],
+//     }, 
+//     // transports: ['websocket', 'polling', 'flashsocket'],
     
-});
+// });
+
+// if (process.env.NODE_ENV === 'development') {
+//     io.engine.on('initial_headers', (headers, req) => {
+//         headers['Access-Control-Allow-Origin'] = 'http://localhost:3000';
+//         headers['Access-Control-Allow-Credentials'] = true;
+//     });
+
+//     io.engine.on('headers', (headers, req) => {
+//         headers['Access-Control-Allow-Origin'] = 'http://localhost:3000';
+//         headers['Access-Control-Allow-Credentials'] = true;
+//     });
+// }
 
 dbConection();
 
@@ -46,33 +55,36 @@ app.get('/', (req,res)=>{
     res.send('Init Page');
 });
 
-io.on('connection',(socket)=>{
-    socket.on("Figures", () => {
-        console.log('Figures',socket.id); // undefined
-        Item.find().exec().then(
-                    x =>{
+// io.on('connection',(socket)=>{
+//     socket.on("Figures", () => {
+//         console.log('Figures',socket.id); // undefined
+//         Item.find().exec().then(
+//                     x =>{
                         
-                        x.sort(function (a, b) {
-                            if (a.name < b.name) {
-                              return -1;
-                            }
-                            if (a.name > b.name) {
-                              return 1;
-                            }
-                            return 0;
-                          });
-                        //   console.log(x);
-                        socket.broadcast.emit("list",x);
-                    } 
-                );
-    });
+//                         x.sort(function (a, b) {
+//                             if (a.name < b.name) {
+//                               return -1;
+//                             }
+//                             if (a.name > b.name) {
+//                               return 1;
+//                             }
+//                             return 0;
+//                           });
+//                         //   console.log(x);
+//                         socket.broadcast.emit("list",x);
+//                     } 
+//                 );
+//     });
 
-    socket.on("disconnect", () => {
-        // console.log('disconnect',socket.id);
-    });
-})
+//     socket.on("disconnect", () => {
+//         // console.log('disconnect',socket.id);
+//     });
+// })
 
-
-server.listen(process.env.PORT,()=>{
+app.listen(process.env.PORT,()=>{
     console.log(`servidor corriendo en puerto ${process.env.PORT}`)
 });
+
+// server.listen(process.env.PORT,()=>{
+//     console.log(`servidor corriendo en puerto ${process.env.PORT}`)
+// });
